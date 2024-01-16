a401fce04c667c0e8a50fb7393877d8cffbc5bb0
minhtuan2001 <minhtuandev2001@gmail.com>
2024-01-16 21:25:37 +0700

    cau truc layout chung cua admin

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 const route = require("./routers/client/index.route");
 const routeAdmin = require("./routers/admin/index.route");
 const connectDB = require('./config/db')
+const systemConfig = require("./config/system")
 
 // // connect db
 connectDB()
@@ -14,6 +15,9 @@ const port = process.env.PORT || 3000
 
 app.set('views', "./views")
 app.set("view engine", "pug")
+
+// App locals variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin
 
 app.use(express.static("public"))
 
