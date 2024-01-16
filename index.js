06b56ff38c05a269f43e4d3991ed5f74ab85abe5
minhtuan2001 <minhtuandev2001@gmail.com>
2024-01-16 15:31:54 +0700

    ket noi database, tao mode product

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express')
+const mongoose = require("mongoose")
 require('dotenv').config()
+
+const route = require("./routers/client/index.route");
+const connectDB = require('./config/db')
+
+// // connect db
+connectDB()
+
 const app = express()
 const port = process.env.PORT || 3000
-const route = require("./routers/client/index.route")
 
-app.use(express.static("public"))
 app.set('views', "./views")
 app.set("view engine", "pug")
+
+app.use(express.static("public"))
 
 // Routes
 route(app)
---
+++ models/product.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+const productSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  discountPercentage: { type: Number },
+  stock: { type: Number },
+  thumbnail: { type: String },
+  status: { type: String },
+  position: { type: Number },
+  deleted: { type: Boolean, default: false }
+})
+
+const Product = mongoose.model('Product', productSchema)
+module.exports = Product
