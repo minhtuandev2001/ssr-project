835980467a08c77c5778d280168e754f01c3238f
minhtuan2001 <minhtuandev2001@gmail.com>
2024-01-26 09:06:34 +0700

    xử lý upload ảnh

--- routers/admin/product.route.js
+++ routers/admin/product.route.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer')
+const storageMulter = require('../../utils/storageMulter')
+const upload = multer({ storage: storageMulter() })
+
 const productController = require("../../controllers/admin/product.controller")
 
 router.get('/', productController.index)
@@ -7,6 +11,6 @@ router.patch('/change-status/:status/:id', productController.changeStatus)
 router.patch('/change-multi', productController.changeMultiStatus)
 router.delete('/delete/:id', productController.deleteProduct)
 router.get('/create', productController.create)
-router.post('/create', productController.createPost)
+router.post('/create', upload.single('thumbnail'), productController.createPost)
 
 module.exports = router
