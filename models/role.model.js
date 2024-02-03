62c42439678e9aaaa63af9bd2316180c726a7906
minhtuan2001 <minhtuandev2001@gmail.com>
2024-02-03 14:49:25 +0700

    tạo model phân quyền

---
+++ models/role.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const roleSchema = mongoose.Schema({
+  title: { type: String },
+  description: { type: String },
+  permissions: { type: Array, default: [] },
+  deleted: { type: Boolean, default: false },
+  deletedAt: { type: Date }
+}, {
+  timestamp: true
+})
+const Role = mongoose.model('Role', roleSchema)
+module.exports = Role
