88301482ec5452a3748f0d8efb1a555973e6218a
minhtuan2001 <minhtuandev2001@gmail.com>
2024-02-06 10:58:38 +0700

    thêm authorization

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,14 +1,18 @@
 const Account = require("../../models/account.model")
+const Role = require("../../models/role.model")
 const systemConfig = require("../../config/system")
 
 const requireAuth = async (req, res, next) => {
   if (!req.cookies?.token) {
     res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
   } else {
-    const user = await Account.findOne({ token: req.cookies.token, deleted: false })
+    const user = await Account.findOne({ token: req.cookies.token, deleted: false }).select("-password")
     if (!user) {
       res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
     } else {
+      const role = await Role.findOne({ _id: user.role_id }).select("title permissions")
+      res.locals.user = user
+      res.locals.role = role
       next()
     }
   }
