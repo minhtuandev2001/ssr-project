bd030714cf92969191d7e4b5ac4bc8b8cc35d426
minhtuan2001 <minhtuandev2001@gmail.com>
2024-02-21 16:23:32 +0700

    làm trang dashboard

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,6 +1,52 @@
-const dashboard = (req, res) => {
+const Product = require("../../models/product.model")
+const ProductCategory = require("../../models/product-category.model")
+const Account = require("../../models/account.model")
+const User = require("../../models/user.model")
+
+// [GET] /admin/dashboard
+const dashboard = async (req, res) => {
   try {
-    res.render("admin/pages/dashboard/index", { titlePage: "admin dashboard" })
+    const statistics = {
+      category: {
+        total: 0,
+        active: 0,
+        inactive: 0,
+      },
+      product: {
+        total: 0,
+        active: 0,
+        inactive: 0,
+      },
+      account: {
+        total: 0,
+        active: 0,
+        inactive: 0,
+      },
+      user: {
+        total: 0,
+        active: 0,
+        inactive: 0,
+      },
+    }
+    statistics.category.total = await ProductCategory.countDocuments({ deleted: false })
+    statistics.category.active = await ProductCategory.countDocuments({ deleted: false, status: "active" })
+    statistics.category.inactive = await ProductCategory.countDocuments({ deleted: false, status: "inactive" })
+
+    statistics.product.total = await Product.countDocuments({ deleted: false })
+    statistics.product.active = await Product.countDocuments({ deleted: false, status: "active" })
+    statistics.product.inactive = await Product.countDocuments({ deleted: false, status: "inactive" })
+
+    statistics.account.total = await Account.countDocuments({ deleted: false })
+    statistics.account.active = await Account.countDocuments({ deleted: false, status: "active" })
+    statistics.account.inactive = await Account.countDocuments({ deleted: false, status: "inactive" })
+
+    statistics.user.total = await User.countDocuments({ deleted: false })
+    statistics.user.active = await User.countDocuments({ deleted: false, status: "active" })
+    statistics.user.inactive = await User.countDocuments({ deleted: false, status: "inactive" })
+    res.render("admin/pages/dashboard/index", {
+      titlePage: "admin dashboard",
+      statistics: statistics
+    })
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: error })
