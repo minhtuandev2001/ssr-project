f6662d333a8efed09d975abad61e0e1d8184a7c3
minhtuan2001 <minhtuandev2001@gmail.com>
2024-03-05 09:44:53 +0700

    list product cho client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -9,7 +9,7 @@ const index = async (req, res) => {
       deleted: false,
       featuread: "1",
       status: "active"
-    }).limit(10)
+    }).limit(4)
     // tính toán giá tiền sau khi khuyến mãi 
     const newProductFeaturead = priceNewDiscountHelper.priceNewDiscountProducts(productFeaturead)
     // kết thúc lấy ra sản phẩm nổi bật
@@ -20,7 +20,7 @@ const index = async (req, res) => {
       status: "active"
     }).sort({
       position: "desc"
-    }).limit(10)
+    }).limit(8)
     // tính toán giá tiền sau khi khuyến mãi 
     const newProductsNew = priceNewDiscountHelper.priceNewDiscountProducts(productsNew)
     // lấy ra sản phẩm mới nhất
